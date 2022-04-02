8bd3373e21c64c6924538d176a501abcde782c25
grobocop12 <chasnik97876@gmail.com>
2022-04-02 14:57:37 +0200

    histogramy stanu kolejki;

--- results/plot_histogram.py
+++ results/plot_histogram.py
@@ -5,19 +5,32 @@ histogram1 = []
 
 with open('histogram1.txt') as f:
     for line in f.readlines():
-        histogram1.append(float(line.split(' ')[-1]))
+        histogram1.append(float(line.split(' ')[-1]) * 100.0)
 
 histogram2 = []
 
 with open('histogram2.txt') as f:
     for line in f.readlines():
-        histogram2.append(float(line.split(' ')[-1]))
+        histogram2.append(float(line.split(' ')[-1]) * 100.0)
 
 x = np.arange(0, len(histogram1))
+
 hist = np.array([x, histogram1])
 
 plt.bar(x, histogram1)
-plt.show()
+plt.xticks(np.arange(0,51,5))
+plt.yticks(np.arange(0,51,5))
+plt.title("Serwer 1")
+plt.xlabel("Stan kolejki")
+plt.ylabel('''% czasu symulacji''')
+plt.grid()
 
+plt.figure()
 plt.bar(x, histogram2)
+plt.xticks(np.arange(0,51,5))
+plt.yticks(np.arange(0,51,5))
+plt.title("Serwer 2")
+plt.xlabel("Stan kolejki")
+plt.ylabel('''% czasu symulacji''')
+plt.grid()
 plt.show()
